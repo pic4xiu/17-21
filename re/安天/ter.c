be6035a50b89791199221c7232d31d7a4c505f46
pic4xiu <40382944+pic4xiu@users.noreply.github.com>
2020-01-14 23:22:06 +0800

    Add files via upload

---
+++ re/安天/ter.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h> 
+#include <stdlib.h> 
+#include <Windows.h> 
+ 
+// 动态调⽤DLL库 
+void DynamicUse() {  
+HMODULE module = LoadLibrary("a.dll");  
+if (module == NULL)  {   return;  }  
+typedef int(*Func)(); // 定义函数指针类型  
+Func test;  test = (Func)GetProcAddress(module, "test"); 
+ 
+ test(); } 
+ 
+int main(char argc, char* argv[]) {  DynamicUse();  system("pause");  return 0; }
